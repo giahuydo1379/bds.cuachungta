c2a5f3497540db08c5a4b7f62e162fe4d0e6d6df
HaLV <halv9988@gmail.com>
2018-11-15 15:42:43 +0700

    update index-v2.blade.php

--- resources/views/frontend/index-v2.blade.php
+++ resources/views/frontend/index-v2.blade.php
@@ -12,7 +12,7 @@
     <section id="page-builder" class="page-section">
         <!-- HERO IMAGE WITH SEARCH FORM -->
         <div class="row tpb-row"
-             style="background-image: url(/html/assets/images/home_hero_image_default.jpg); padding-top: 144px; padding-bottom: 168px; margin: 0; background-size: cover; background-position: center;">
+             style="margin: 0; padding-top: 40px;">
             <div class="tpb tpb-property_simple_search col-md-12">
                 <div class="container">
                     <?=\App\Helpers\Block::form_search();?>
@@ -61,36 +61,6 @@
                         <?=\App\Helpers\Block::news_item($article)?>
                     </div>
             @endforeach
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- ====== TESTIMONY SECTION ====== -->
-    <section id="testimony" class="page-section">
-        <div class="container">
-            <!-- Section Header / Title with Column Slider Control / Add 'header-column' to use this style -->
-            <div class="section-header header-column">
-                <h2 class="section-title"><a href="{{route('fe.article.fengshui')}}" title="">Phong thủy</a> <a class="btn-more" href="{{route('fe.article.fengshui')}}">Xem thêm <i
-                                class="fa fa-angle-double-right" aria-hidden="true"></i></a></h2>
-                <!-- Slider Control -->
-                <div class="slide-control">
-                    <button id="content-left" class="slide-left content-left"><i class="fa fa-angle-left"></i></button>
-                    <button id="content-right" class="slide-right content-right"><i class="fa fa-angle-right"></i></button>
-                </div>
-            </div>
-            <!-- Testimony Slider Content -->
-            <div id="content-slider" class="content-slider testimony-wrapper">
-                <!-- Testimony Slider Item -->
-                @foreach($fengshuis->chunk(2) as $chunk)
-                <div class="slider-item">
-                    <!-- Testimony Left -->
-                    @foreach($chunk as $key=>$item)
-                        <?=\App\Helpers\Block::fengshui_item($item, $key%2==0?'item-left':'item-right')?>
-                    @endforeach
-                    <!-- Testimony Right -->
-                </div>
-                @endforeach
-
                 </div>
             </div>
         </div>
